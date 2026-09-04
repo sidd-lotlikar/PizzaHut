dac75a9e84323e76eb19afd3698b9f3f26605f35
sidd-lotlikar <sidd_lotlikar@outlook.com>
2026-09-04 13:41:54 -0500

    feat(Main): Adding sandbox code to test FileHandle's end-to-end flow .

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <filesystem>
+#include <vector>
 
 int main() {
     const std::filesystem::path directoryPath = std::filesystem::temp_directory_path() / "PizzaHut";
@@ -12,10 +13,32 @@ int main() {
     }
     // Create file path and test open.
     const std::filesystem::path filePath = directoryPath / "a.txt";
-    auto fileHandle = FileHandle::Open(filePath.c_str());
-    if (fileHandle.IsOk()) {
-        std::cout << "successfully created a file!" << std::endl;
-    } else {
-        std::cout << "Open Error" << std::endl;
+    
+    // Create the file
+    auto opened = FileHandle::Open(filePath.c_str());
+    if (!opened.IsOk()) {
+        std::cerr << "opening the file failed\n";
     }
+
+    auto& file = opened.value();
+    const std::string message = "Veggie Lover";
+
+    auto written = file.Write(
+        0, 
+        reinterpret_cast<const std::byte*>(message.data()), 
+        message.size()
+    );
+
+    if (!written.IsOk()) {
+        std::cerr << "Write failed\n";
+    }
+
+    std::vector<std::byte> buffer(message.size());
+    auto read = file.Read(0, buffer.data(), buffer.size());
+    if (!read.IsOk()) {
+        std::cerr << "Read failed\n";
+    }
+
+    std::string result(reinterpret_cast<const char*>(buffer.data()), read.value());
+    std::cout << "Read: " << result << "\n";
 }
